951d1912007315e77fe919e5226fbb7f634f9684
JoanJimenez <37439060+JoanJimenez@users.noreply.github.com>
2018-03-18 23:09:45 +0100

    Add files via upload
    Afegits versions finals fitxers tasques 7 i 8
    Afegit programa tasca 10, client i servidor unificat

---
+++ P2/t7-clientTCP.py
@@ -0,0 +1,27 @@
+# Echo client program
+import socket
+import sys
+
+HOST = '172.20.13.1'    # The remote host
+PORT = 10000              # The same port as used by the server
+s = None
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        s = None
+        continue
+    try:
+        s.connect(sa)
+    except socket.error as msg:
+        s.close()
+        s = None
+        continue
+    break
+if s is None:
+    print 'could not open socket'
+    sys.exit(1)
+s.sendall('Missatge de prova')
+data = s.recv(1024)
+s.close()
---
+++ P2/t8-servidorUDP.py
@@ -0,0 +1,15 @@
+# Echo server program
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 10000              # Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((HOST, PORT))
+
+while 1:
+    data,addr = s.recvfrom(1024)
+    if not data: break
+    s.sendto('Missatge de prova del server',addr)
+    print "Missatge entrant: "+str(addr)+" "+data
+conn.close()
